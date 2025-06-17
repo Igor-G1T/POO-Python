c9bd8f0484372f28d731af8b607513dd52805478
Igor-G1T <igor.gabriel@guaraves.com.br>
2025-06-17 11:18:57 -0300

    Primeiro Commit

---
+++ OO-SABOR-EXPRESS/modelos/musica.py
@@ -0,0 +1,10 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = int
+
+musica1 = Musica()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.duracao = 355
+
+print(f' Música: {musica1.nome}\n Banda: {musica1.artista}\n duração: {musica1.duracao} segundos')
