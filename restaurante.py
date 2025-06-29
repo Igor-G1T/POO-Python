f72c83ffa2b7a5b5b83e55ff988f510e088bff50
Igor-G1T <igor.gabriel@guaraves.com.br>
2025-06-29 12:52:11 -0300

    Add files via upload

---
+++ restaurante.py
@@ -0,0 +1,37 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+restaurante_praca = Restaurante('Praca', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
+
+Restaurante.listar_restaurantes()
+
+
+'''
+Exercícios
+
+1. Implemente uma classe chamada Carro com os atributos básicos, como modelo, cor e ano. Crie uma instância dessa classe e atribua valores aos seus atributos.
+
+2. Crie uma classe chamada Restaurante com os atributos nome, categoria, ativo e crie mais 2 atributos. Instancie um restaurante e atribua valores aos seus atributos.
+
+3. Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros e inicia ativo como False por padrão. Crie uma instância utilizando o construtor.
+
+4. Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância, seja exibida uma mensagem formatada com o nome e a categoria. Exiba essa mensagem para uma instância de restaurante.
+
+5. Crie uma classe chamada Cliente e pense em 4 atributos. Em seguida, instancie 3 objetos desta classe e atribua valores aos seus atributos através de um método construtor.
+'''
