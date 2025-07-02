f9519dd4645f7b7b2ed66919d0bc1950810bf8f8
Igor-G1T <igor.gabriel@guaraves.com.br>
2025-07-02 12:59:20 -0300

    Add files via upload

--- OO-SABOR-EXPRESS/modelos/musica.py
+++ OO-SABOR-EXPRESS/modelos/musica.py
@@ -1,9 +1,10 @@
 class Musica:
-    def __init__(self, nome='', artista='', duracao=0):
-        self.nome = nome
-        self.artista = artista
-        self.duracao = duracao
+    nome = ''
+    artista = ''
+    duracao = int
 
-musica1 = Musica(nome='Under Pressure', artista='Queen & David Bowie', duracao=248)
-musica2 = Musica(nome='The Trooper', artista='Iron Maiden', duracao=245)
-musica3 = Musica(nome='Hotel California', artista='Eagles', duracao=390)
+musica1 = Musica()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.duracao = 355
+
+print(f' Música: {musica1.nome}\n Banda: {musica1.artista}\n duração: {musica1.duracao} segundos')
